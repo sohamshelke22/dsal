4f6ed5c8a0e4deb032dda5e3e84ae78a6985303e
sohamshelke22 <signupforsoham@gmail.com>
2025-05-06 13:25:01 +0530

    Create Priority.cpp

---
+++ Priority.cpp
@@ -0,0 +1,129 @@
+#include<iostream>
+#include<string>
+
+#define N 20
+
+#define SERIOUS 10
+#define NONSERIOUS 5
+#define CHECKUP 1
+
+using namespace std;
+string Q[N];  //Size of Queue
+int Pr[N];   //FOR CATAGORY OF Patient 
+int r = -1,f = -1;
+void enqueue(string name,int p)//Enqueue function to insert data and its priority in queue
+{
+	int i;
+	if((f==0)&&(r==N-1)) //Check if Queue is full
+		cout<<"Queue is full";
+	else {
+		if(f==-1) { //if Queue is empty
+			f = r = 0;
+			Q[r] = name;         //FIRST ENTRY
+			Pr[r] = p;           //FIRST ENTRY
+
+		}
+	
+		else {      //second patient onwards
+			for(i = r;i>=f;i--) {
+				if(p>Pr[i]) //if priority of the new patient is > exixting patient then shift the existing patient one place right
+				{
+					Q[i+1] = Q[i];
+					Pr[i+1] = Pr[i];	
+				}
+				else break;
+			}
+			Q[i+1] = name;
+			Pr[i+1] = p;
+			r++;
+		}	
+	}
+
+}
+void print() { //print the data of Queue
+	int i;
+	for(i=f;i<=r;i++) {
+		cout << "Patient's Name - "<<Q[i];
+		switch(Pr[i]) {
+			case 1: 
+				cout << " Priority - 'Checkup' " << endl;
+			break;
+			case 5:
+				cout << " Priority - 'Non-serious' " << endl;
+			break;
+			case 10:
+				cout << " Priority - 'Serious' " << endl;
+			break;
+			default:
+				cout << "Priority not found" << endl;
+		}
+	}
+}
+
+void dequeue() { //remove the data from front
+	if(f == -1) {
+		cout<<"Queue is Empty";
+	}	
+	else {
+	cout<<"deleted Element ="<<Q[f]<<endl;
+	
+		if(f==r) f = r = -1;
+		else f++;
+	}
+}
+
+int main() 
+{
+	string name;
+	int opt,n,i,p;
+	cout<<"Enter Your Choice:-"<<endl;
+while(1) 
+ {
+	cout << "\nMenu \n 1)Insert Data in Queue" << endl << " 2)show the Data in Queue " << endl <<" 3)Delete data from the Queue" 
+		<< endl <<" 4)Exit"<< endl;
+	cin >> opt;
+		switch(opt) 
+		{
+			case 1:
+				cout << "Enter the number of patinent" << endl;
+				cin >> n;
+				i = 0;
+				while(i < n) 
+				{
+					cout << "Enter your name of the patient : ";
+					cin >> name;
+				
+						cout << "Enter your Prioritys (0: Serious, 1: Non-serious, 2: General checkup) : ";
+						cin >> p;
+						switch(p) {
+							case 0: 
+								enqueue(name,SERIOUS);
+							break;
+							case 1: 
+								enqueue(name,NONSERIOUS);
+							break;
+							case 2: 
+								enqueue(name,CHECKUP);
+							break;
+							
+						}
+						
+					i++;
+				}
+			break;
+			case 2:
+				print();
+			break;
+			case 3:
+				 dequeue();
+			break;
+			case 4:
+				
+			exit(0);
+			default:
+			cout<<"Incorrect Choice"<<endl;
+
+		}
+	}
+        return 0;
+}
